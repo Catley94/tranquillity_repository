8b663ae4a13857c38d36a55757c1a2d81f310f8a
Sam Catley <scp94373@gmail.com>
2021-02-14 10:11:32 +0000

    corrected database name

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm tranquillity_repo*
+rm tranquillity_repository*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R tranquillity_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R tranquillity_repository.db.tar.gz *.pkg.tar.zst
 #repo-add -s -n -R tranquillity-repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
